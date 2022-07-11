4e984b7bcc40d7d8218cc8407f0bab424da15abc
Markus Persson <markusperssonbiz@gmail.com>
2022-07-11 12:06:22 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menu");
+const links = document.querySelector(".links");
+
+menu.addEventListener("click", function () {
+  links.classList.toggle("active");
+});
