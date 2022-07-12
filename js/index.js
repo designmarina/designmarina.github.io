3a8496e11b1217ffc3d28c9bb8a4225da4fb3e8d
Markus Persson <markusperssonbiz@gmail.com>
2022-07-12 15:16:19 +0200

    v1.0.5

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
 const menu = document.querySelector(".menu");
 const links = document.querySelector(".links");
+const header = document.querySelector("header");
 
 menu.addEventListener("click", function () {
   links.classList.toggle("active");
 });
+
+var modal = document.getElementById("modal");
+
+var modalClose = document.getElementById("modal-close");
+modalClose.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.className.indexOf("modal-target") !== -1) {
+    var img = e.target;
+    var modalImg = document.getElementById("modal-content");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  }
+});
+
+window.onclick = (e) => {
+  if (e.target.className == "modal") {
+    modal.style.display = "none";
+  }
+};
